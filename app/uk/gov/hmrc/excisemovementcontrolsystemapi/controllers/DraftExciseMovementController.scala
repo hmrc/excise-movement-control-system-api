7393e413de45b9dc8085595604b1111a6909cf03
solomon.amadi <solomon.amadi@digital.hmrc.gov.uk>
2023-09-19 10:45:17 +0100

    Updated response message

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/DraftExciseMovementController.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/DraftExciseMovementController.scala
@@ -39,7 +39,8 @@ class DraftExciseMovementController @Inject()(
   def submit: Action[NodeSeq] =
     (authAction andThen xmlParser andThen consignorValidatorAction).async(parse.xml) { implicit request: AuthorizedIE815Request[NodeSeq] =>
       // todo integrate controller
+      // todo store MovementDetails(LRN, ConsignorId, ConsigneeId) in mongodb
       // movementMessageConnector.post("test-message", "IE815")
-      Future.successful(Ok(Json.parse("""{"name": "mauro"}""")))
+      Future.successful(Ok(Json.parse("""{"message": "Draft Movement submitted successfully"}""")))
     }
 }
