30590114c5a5a0c5714d3167b642426e4b837e12
mauro-fanton <mauro.fanton@opencastsoftware.com>
2023-09-27 09:32:59 +0100

    instantiating clock

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/config/Module.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/config/Module.scala
@@ -18,10 +18,13 @@ package uk.gov.hmrc.excisemovementcontrolsystemapi.config
 
 import com.google.inject.AbstractModule
 
+import java.time.{Clock, ZoneOffset}
+
 class Module extends AbstractModule {
 
   override def configure(): Unit = {
 
     bind(classOf[AppConfig]).asEagerSingleton()
+    bind(classOf[Clock]).toInstance(Clock.systemDefaultZone.withZone(ZoneOffset.UTC))
   }
 }
