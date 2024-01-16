43295856054124ad98091e6d0f0b4fea145b2fd7
Chris Cook <114592674+ChrisCookOC@users.noreply.github.com>
2024-01-16 13:40:33 +0000

    Get Movement Controller

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/GetMovementController.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/GetMovementController.scala
@@ -1,8 +1,11 @@
 package uk.gov.hmrc.excisemovementcontrolsystemapi.controllers
 
+import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import uk.gov.hmrc.excisemovementcontrolsystemapi.controllers.actions.AuthAction
+import uk.gov.hmrc.excisemovementcontrolsystemapi.controllers.actions.{AuthAction, ValidateMovementIdAction}
+import uk.gov.hmrc.excisemovementcontrolsystemapi.models.ErrorResponse
 import uk.gov.hmrc.excisemovementcontrolsystemapi.services.MovementService
+import uk.gov.hmrc.excisemovementcontrolsystemapi.utils.DateTimeService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 import javax.inject.Inject
@@ -11,16 +14,24 @@ import scala.concurrent.ExecutionContext
 class GetMovementController @Inject()(
                                        authAction: AuthAction,
                                        movementService: MovementService,
+                                       dateTimeService: DateTimeService,
                                        cc: ControllerComponents
                                      )(implicit ec: ExecutionContext)
   extends BackendController(cc) {
 
   def getMovement(movementId: String): Action[AnyContent] = {
 
-    authAction.async(parse.default) {
+    (authAction).async(parse.default) {
       implicit request => movementService.getMovementById(movementId).map {
-      case Some(_) => Ok("ok :)")
-      case None => BadRequest("sad :(") //TODO error
+      case Some(_) => {
+
+        Ok("ok :)")
+      }
+      case None => NotFound(Json.toJson(ErrorResponse(
+        dateTimeService.timestamp(),
+        "Movement not found",
+        s"Movement $movementId is not found"
+      )))
     }}
 
   }
--- test/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/GetMovementControllerSpec.scala
+++ test/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/GetMovementControllerSpec.scala
@@ -2,28 +2,48 @@ package uk.gov.hmrc.excisemovementcontrolsystemapi.controllers
 
 import dispatch.Future
 import org.mockito.ArgumentMatchersSugar.eqTo
-import org.mockito.MockitoSugar.when
+import org.mockito.MockitoSugar.{reset, when}
+import org.scalatest.BeforeAndAfterEach
 import org.scalatestplus.mockito.MockitoSugar.mock
 import org.scalatestplus.play.PlaySpec
-import play.api.http.Status.{FORBIDDEN, OK}
+import play.api.http.Status.{FORBIDDEN, NOT_FOUND, OK}
+import play.api.libs.json.Json
 import play.api.test.FakeRequest
-import play.api.test.Helpers.{defaultAwaitTimeout, status, stubControllerComponents}
-import uk.gov.hmrc.excisemovementcontrolsystemapi.fixture.FakeAuthentication
+import play.api.test.Helpers.{contentAsJson, defaultAwaitTimeout, status, stubControllerComponents}
+import uk.gov.hmrc.excisemovementcontrolsystemapi.fixture.{FakeAuthentication, FakeValidateMovementIdAction}
+import uk.gov.hmrc.excisemovementcontrolsystemapi.models.ErrorResponse
 import uk.gov.hmrc.excisemovementcontrolsystemapi.repository.model.Movement
 import uk.gov.hmrc.excisemovementcontrolsystemapi.services.MovementService
+import uk.gov.hmrc.excisemovementcontrolsystemapi.utils.DateTimeService
 
 import java.time.Instant
 import scala.concurrent.ExecutionContext
 
 class GetMovementControllerSpec
   extends PlaySpec
-    with FakeAuthentication {
+    with FakeAuthentication
+    with FakeValidateMovementIdAction
+    with BeforeAndAfterEach {
   implicit val ec: ExecutionContext = ExecutionContext.Implicits.global
   private val cc = stubControllerComponents()
   private val fakeRequest = FakeRequest("POST", "/foo")
   private val movementService = mock[MovementService]
+  private val dateTimeService = mock[DateTimeService]
+  private val timestamp = Instant.parse("2018-11-30T18:35:24.00Z")
 
-  private val controller = new GetMovementController(FakeSuccessAuthentication, movementService, cc)
+  private val controller = new GetMovementController(
+    FakeSuccessAuthentication,
+    movementService,
+    cc
+  )
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+
+    reset(movementService)
+    when(dateTimeService.timestamp()).thenReturn(timestamp)
+  }
+
 
   "Get movement controller" should {
 
@@ -37,6 +57,21 @@ class GetMovementControllerSpec
 
       status(result) mustBe OK
 
+    }
+
+    "return Not Found error" when {
+      "movement not found in database" in {
+
+        when(movementService.getMovementById(eqTo("id123"))).thenReturn(Future.successful(None))
+       val result = controller.getMovement("id123")(fakeRequest)
+
+        status(result) mustBe NOT_FOUND
+
+        contentAsJson(result) mustBe Json.toJson(
+          ErrorResponse(timestamp, "Movement not found", "Movement id123 is not found")
+        )
+
+      }
     }
 
     "return authentication error" when {
@@ -54,5 +89,6 @@ class GetMovementControllerSpec
     movementService,
     cc
   )
+
 
 }
