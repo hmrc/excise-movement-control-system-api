69975bed135c6334246dd2b25a729c9bca1a6450
mauro-fanton <mauro.fanton@opencastsoftware.com>
2024-01-16 11:22:58 +0000

    refactor

--- it/uk/gov/hmrc/excisemovementcontrolsystemapi/DraftExciseMovementControllerItSpec.scala
+++ it/uk/gov/hmrc/excisemovementcontrolsystemapi/DraftExciseMovementControllerItSpec.scala
@@ -18,7 +18,6 @@ package uk.gov.hmrc.excisemovementcontrolsystemapi
 
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock._
-import org.mockito.MockitoSugar.{reset => MockitSugerReset}
 import org.bson.types.ObjectId
 import org.mockito.ArgumentMatchersSugar.any
 import org.mockito.MockitoSugar.{when, reset => mockitoSugerReset}
@@ -50,9 +49,7 @@ import uk.gov.hmrc.mongo.workitem.{ProcessingStatus, WorkItem}
 
 import java.time.Instant
 import java.time.temporal.ChronoUnit
-import java.util.UUID
 import java.time.{Instant, LocalDateTime, ZoneOffset, ZonedDateTime}
-import scala.concurrent.duration.MINUTES
 import scala.concurrent.{ExecutionContext, Future}
 import scala.xml.NodeSeq
 
