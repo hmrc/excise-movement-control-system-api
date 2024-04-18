d3fe8dfbd63307e2dc1ea67b9fe462822e5ac40e
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2024-04-18 08:43:39 +0100

    Move JobScheduler to its own file and inline code from RunningOfScheduledJobs to simplify code

---
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/config/JobScheduler.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.excisemovementcontrolsystemapi.config
+
+import org.apache.commons.lang3.time.StopWatch
+import org.apache.pekko.actor.ActorSystem
+import play.api.Logging
+import play.api.inject.ApplicationLifecycle
+import uk.gov.hmrc.excisemovementcontrolsystemapi.scheduling.{PollingNewMessagesWithWorkItemJob, ScheduledJob}
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+@Singleton
+class JobScheduler @Inject() (
+                               pollingNewMessageWorkItemJob: PollingNewMessagesWithWorkItemJob,
+                               applicationLifecycle: ApplicationLifecycle,
+                               actorSystem: ActorSystem
+                             )(implicit val ec: ExecutionContext) extends Logging {
+
+  private val scheduledJobs: Seq[ScheduledJob] = Seq(
+    pollingNewMessageWorkItemJob
+  ).filter(_.enabled)
+
+  private val cancellables = scheduledJobs.map { job =>
+    actorSystem.scheduler.scheduleWithFixedDelay(job.initialDelay, job.intervalBetweenJobRunning) { () =>
+      val stopWatch = new StopWatch
+      stopWatch.start()
+      logger.info(s"Executing job ${job.name}")
+
+      job.execute.onComplete {
+        case Success(job.Result(message)) =>
+          stopWatch.stop()
+          logger.info(s"Completed job ${job.name} in $stopWatch: $message")
+        case Failure(throwable) =>
+          stopWatch.stop()
+          logger.error(s"Exception running job ${job.name} after $stopWatch", throwable)
+      }
+    }
+  }
+
+  applicationLifecycle.addStopHook(() => {
+    logger.info(s"Cancelling all scheduled jobs.")
+    cancellables.foreach(_.cancel())
+    Future.unit
+  })
+}
