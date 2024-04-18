5d094d28c0abfb97ea73a4dd4264efd867d6aa7f
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2024-04-18 08:59:17 +0100

    Add metrics to job scheduling

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/config/JobScheduler.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/config/JobScheduler.scala
@@ -16,12 +16,14 @@
 
 package uk.gov.hmrc.excisemovementcontrolsystemapi.config
 
-import org.apache.commons.lang3.time.StopWatch
+import com.codahale.metrics.Timer
 import org.apache.pekko.actor.ActorSystem
 import play.api.Logging
 import play.api.inject.ApplicationLifecycle
 import uk.gov.hmrc.excisemovementcontrolsystemapi.scheduling.{PollingNewMessagesWithWorkItemJob, ScheduledJob}
+import uk.gov.hmrc.play.bootstrap.metrics.Metrics
 
+import java.time.{Clock, Duration}
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
@@ -30,26 +32,32 @@ import scala.util.{Failure, Success}
 class JobScheduler @Inject() (
                                pollingNewMessageWorkItemJob: PollingNewMessagesWithWorkItemJob,
                                applicationLifecycle: ApplicationLifecycle,
-                               actorSystem: ActorSystem
+                               actorSystem: ActorSystem,
+                               metrics: Metrics,
+                               clock: Clock
                              )(implicit val ec: ExecutionContext) extends Logging {
 
   private val scheduledJobs: Seq[ScheduledJob] = Seq(
     pollingNewMessageWorkItemJob
   ).filter(_.enabled)
 
+  private val timers: Map[ScheduledJob, Timer] = scheduledJobs.map { job =>
+    job -> metrics.defaultRegistry.timer(s"${job.name}.timer")
+  }.toMap
+
   private val cancellables = scheduledJobs.map { job =>
     actorSystem.scheduler.scheduleWithFixedDelay(job.initialDelay, job.intervalBetweenJobRunning) { () =>
-      val stopWatch = new StopWatch
-      stopWatch.start()
+      val startTime = clock.instant()
       logger.info(s"Executing job ${job.name}")
-
-      job.execute.onComplete {
-        case Success(job.Result(message)) =>
-          stopWatch.stop()
-          logger.info(s"Completed job ${job.name} in $stopWatch: $message")
-        case Failure(throwable) =>
-          stopWatch.stop()
-          logger.error(s"Exception running job ${job.name} after $stopWatch", throwable)
+      job.execute.onComplete { result =>
+        val duration = Duration.between(startTime, clock.instant())
+        timers(job).update(duration)
+        result match {
+          case Success(job.Result(message)) =>
+            logger.info(s"Completed job ${job.name} in ${duration.toSeconds}s: $message")
+          case Failure(throwable) =>
+            logger.error(s"Exception running job ${job.name} after ${duration.toSeconds}s", throwable)
+        }
       }
     }
   }
