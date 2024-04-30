621ec6d3950cc89d74071d7c26bac61ba340b955
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2024-04-30 15:13:16 +0100

    adding ern retrieval repository

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/repository/ErnRetrievalRepository.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/repository/ErnRetrievalRepository.scala
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.excisemovementcontrolsystemapi.repository
 
 import org.apache.pekko.Done
-import org.mongodb.scala.model.{IndexModel, IndexOptions, Indexes}
+import org.apache.pekko.Done.done
+import org.mongodb.scala.model.{Filters, IndexModel, IndexOptions, Indexes, ReplaceOptions}
 import play.api.Logging
 import uk.gov.hmrc.excisemovementcontrolsystemapi.config.AppConfig
 import uk.gov.hmrc.excisemovementcontrolsystemapi.repository.ErnRetrievalRepository.mongoIndexes
@@ -10,6 +27,7 @@ import uk.gov.hmrc.excisemovementcontrolsystemapi.utils.DateTimeService
 import uk.gov.hmrc.mongo.MongoComponent
 import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
 
+import java.time.Instant
 import java.util.concurrent.TimeUnit
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.duration.Duration
@@ -21,16 +39,25 @@ class ErnRetrievalRepository @Inject()
  appConfig: AppConfig,
  timeService: DateTimeService)(implicit ec: ExecutionContext) extends
   PlayMongoRepository[ErnRetrieval](
-    collectionName = "ern-retrievals",
+    collectionName = "ernretrievals",
     mongoComponent = mongo,
     domainFormat = ErnRetrieval.format,
     indexes = mongoIndexes(appConfig.ernRetrievalTTL),
     replaceIndexes = true
   ) with Logging {
 
-  def retrieve(ern: String): Future[Option[ErnRetrieval]] = ???
+  def getLastRetrieved(ern: String): Future[Option[Instant]] =
+    collection.find(Filters.eq("ern", ern))
+      .headOption()
+      .map(r => r.map(_.lastRetrieved))
 
-  def save(ern: String): Future[Done] = ???
+  def save(ern: String): Future[Done] = {
+    collection.replaceOne(
+      Filters.eq("ern", ern),
+      ErnRetrieval(ern, timeService.timestamp()),
+      ReplaceOptions().upsert(true)
+    ).toFuture().map(_ => done())
+  }
 }
 
 object ErnRetrievalRepository {
