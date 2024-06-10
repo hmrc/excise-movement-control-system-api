24149447a652711c8d465d0403f05e2f2d33fc30
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2024-06-10 15:21:47 +0100

    [EMCS-567] remove further config items not needed

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/config/AppConfig.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/config/AppConfig.scala
@@ -20,7 +20,7 @@ import play.api.Configuration
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.duration.{DAYS, Duration, FiniteDuration, HOURS, MINUTES, SECONDS}
+import scala.concurrent.duration.{DAYS, Duration, FiniteDuration}
 
 @Singleton
 class AppConfig @Inject() (config: Configuration, servicesConfig: ServicesConfig) {
@@ -35,58 +35,12 @@ class AppConfig @Inject() (config: Configuration, servicesConfig: ServicesConfig
   lazy val nrsRetryDelays: Seq[FiniteDuration] =
     config.get[Seq[FiniteDuration]]("microservice.services.nrs.retryDelays")
 
-  lazy val interval: FiniteDuration = config
-    .getOptional[String]("scheduler.pollingNewMessageJob.interval")
-    .map(Duration.create(_).asInstanceOf[FiniteDuration])
-    .getOrElse(FiniteDuration(1, MINUTES))
-
-  lazy val initialDelay: FiniteDuration = config
-    .getOptional[String]("scheduler.pollingNewMessageJob.initialDelay")
-    .map(Duration.create(_).asInstanceOf[FiniteDuration])
-    .getOrElse(FiniteDuration(60, SECONDS))
-
-  lazy val workItemInProgressTimeOut: FiniteDuration = config
-    .getOptional[String]("scheduler.workItems.inProgressTimeOut")
-    .map(Duration.create(_).asInstanceOf[FiniteDuration])
-    .getOrElse(FiniteDuration(5, MINUTES))
-
-  // When a work item fails, this is how many times to retry it before permanently failing
-  lazy val maxFailureRetryAttempts: Int =
-    config.getOptional[Int]("scheduler.workItems.failureRetryAttempts").getOrElse(3)
-
-  //How long after a failure should we wait before retrying it?
-  // (It will only be retried if it was BOTH last updated before Now-failureRetryAfter and availableAt before Now)
-  lazy val failureRetryAfter: FiniteDuration = config
-    .getOptional[String]("scheduler.workItems.failureRetryAfter")
-    .map(Duration.create(_).asInstanceOf[FiniteDuration])
-    .getOrElse(FiniteDuration(5, MINUTES))
-
-  // When on the fast interval, how many times to fast poll before switching to slow poll
-  lazy val fastIntervalRetryAttempts: Int =
-    config.getOptional[Int]("scheduler.workItems.fastIntervalRetryAttempts").getOrElse(3)
-
-  // The fast interval is used after submitting a message
-  lazy val workItemFastInterval: FiniteDuration = config
-    .getOptional[String]("scheduler.workItems.fastInterval")
-    .map(Duration.create(_).asInstanceOf[FiniteDuration])
-    .getOrElse(FiniteDuration(5, MINUTES))
-
-  // The slow interval is used once fastInterval has been used for fastIntervalRetryAttempts times
-  lazy val workItemSlowInterval: FiniteDuration = config
-    .getOptional[String]("scheduler.workItems.slowInterval")
-    .map(Duration.create(_).asInstanceOf[FiniteDuration])
-    .getOrElse(FiniteDuration(1, HOURS))
-
   lazy val movementTTL: Duration = config
     .getOptional[String]("mongodb.movement.TTL")
     .fold(Duration.create(30, DAYS))(Duration.create(_).asInstanceOf[FiniteDuration])
 
   lazy val ernRetrievalTTL: Duration = config
     .getOptional[String]("mongodb.ernRetrieval.TTL")
-    .fold(Duration.create(30, DAYS))(Duration.create(_).asInstanceOf[FiniteDuration])
-
-  lazy val workItemTTL: Duration = config
-    .getOptional[String]("mongodb.workItem.TTL")
     .fold(Duration.create(30, DAYS))(Duration.create(_).asInstanceOf[FiniteDuration])
 
   lazy val pushNotificationsEnabled: Boolean = servicesConfig.getBoolean("featureFlags.pushNotificationsEnabled")
