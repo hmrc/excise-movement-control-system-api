c70413d9cc5a94d7427a5b839398a232d1ebb0b4
JoeSimmonds <884957+JoeSimmonds@users.noreply.github.com>
2024-08-07 11:38:38 +0100

    moved bootstrap to v9.2.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.4.0"
+  private val bootstrapVersion = "9.2.0"
   val dispatchVersion        = "1.2.0"
   lazy val mongoVersion = "1.7.0"
-
+git checkout
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-work-item-repo-play-30" % mongoVersion,
-    "javax.xml.bind"          % "jaxb-api"                    % "2.3.1",
-    "com.beachape"            %% "enumeratum-play-json"       % "1.8.0",
-    "org.typelevel"           %% "cats-core"                  % "2.10.0",
-    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "3.0.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"          % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-work-item-repo-play-30"  % mongoVersion,
+    "javax.xml.bind"          % "jaxb-api"                            % "2.3.1",
+    "com.beachape"            %% "enumeratum-play-json"               % "1.8.0",
+    "org.typelevel"           %% "cats-core"                          % "2.10.0",
+    "uk.gov.hmrc"             %% "internal-auth-client-play-30"       % "3.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
