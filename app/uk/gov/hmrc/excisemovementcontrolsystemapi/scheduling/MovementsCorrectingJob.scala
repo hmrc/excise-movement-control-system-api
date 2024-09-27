6916c4da203039c8cfae91088075cd48acd7c46b
Jake Goodwin <12535866+JakeGoodwin@users.noreply.github.com>
2024-09-27 15:02:00 +0100

    Added initial tests

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/scheduling/MovementsCorrectingJob.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/scheduling/MovementsCorrectingJob.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.excisemovementcontrolsystemapi.scheduling
 
 import cats.implicits.toFunctorOps
@@ -5,6 +21,7 @@ import org.apache.pekko.Done
 import play.api.{Configuration, Logging}
 import uk.gov.hmrc.excisemovementcontrolsystemapi.repository.ProblemMovementsWorkItemRepo
 import uk.gov.hmrc.excisemovementcontrolsystemapi.services.MessageService
+import uk.gov.hmrc.excisemovementcontrolsystemapi.utils.DateTimeService
 import uk.gov.hmrc.http.HeaderCarrier
 
 import java.time.Instant
@@ -17,14 +34,15 @@ import scala.concurrent.{ExecutionContext, Future}
 class MovementsCorrectingJob @Inject() (
   configuration: Configuration,
   messageService: MessageService,
-  problemMovementsWorkItemRepo: ProblemMovementsWorkItemRepo
+  problemMovementsWorkItemRepo: ProblemMovementsWorkItemRepo,
+  dateTimeService: DateTimeService
 )(implicit hc: HeaderCarrier)
     extends ScheduledJob
     with Logging {
   override def name: String = "movements-correcting-job"
 
   override def execute(implicit ec: ExecutionContext): Future[ScheduledJob.Result] = {
-    val now = Instant.now()
+    val now = dateTimeService.timestamp()
 
     problemMovementsWorkItemRepo
       .pullOutstanding(failedBefore = now.minus(1, ChronoUnit.DAYS), now)
@@ -38,6 +56,9 @@ class MovementsCorrectingJob @Inject() (
   }
 
   override val enabled: Boolean = configuration.get[Boolean]("featureFlags.movementsCorrectingEnabled")
+
+  override val numberOfInstances: Int =
+    configuration.get[Int]("scheduler.movementsCorrectingEnabled.numberOfInstances")
 
   override def initialDelay: FiniteDuration =
     configuration.get[FiniteDuration]("scheduler.movementsCorrectingJob.initialDelay")
