eec4e731428add3f5272546ff51ebce8c908c93d
Nicole Avison <Froom2@users.noreply.github.com>
2024-10-25 14:57:41 +0100

    updating error logging

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/GetMovementsController.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/GetMovementsController.scala
@@ -74,7 +74,7 @@ class GetMovementsController @Inject() (
           movement <- movementService.getMovementByErn(request.erns.toSeq, filter)
         } yield Ok(Json.toJson(movement.map(createResponseFrom)))
       }.recover { case NonFatal(ex) =>
-        logger.warn("Error getting movements", ex)
+        logger.warn(s"Error getting movements for erns ${request.erns} with filters ern: $ern, lrn: $lrn, arc: $arc, updatedSince: $updatedSince, traderType: $traderType", ex)
         InternalServerError(
           Json.toJson(
             ErrorResponse(
