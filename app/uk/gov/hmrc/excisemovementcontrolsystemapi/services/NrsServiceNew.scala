f07fcbe4bab806f91487a3a279b5a20cc293111b
Nicole Avison <Froom2@users.noreply.github.com>
2024-11-13 16:06:07 +0000

    adding logging for workitem handling

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/services/NrsServiceNew.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/services/NrsServiceNew.scala
@@ -82,15 +82,17 @@ class NrsServiceNew @Inject() (
     //needs to call connector, and handle marking the workitems. IN PROGRESS
 
     nrsConnectorNew.sendToNrs(workItem.item.payload, correlationIdService.generateCorrelationId())
-      .flatMap( _ =>
+      .flatMap( _ => {
         nrsWorkItemRepository.complete(workItem.id, Succeeded)
           .map(_ => Done)
-      )
+      })
       .recoverWith {
         case UnexpectedResponseException(status, _) if is4xx(status) =>
+          logger.warn(s"NRS call failed permanently with status $status - marking workitem as PermanentlyFailed")
           nrsWorkItemRepository.complete(workItem.id, PermanentlyFailed)
             .map(_ => Done)
         case UnexpectedResponseException(status, _) if is5xx(status) =>
+          logger.warn(s"NRS call failed with status $status - marking workitem as Failed for retry")
           nrsWorkItemRepository.complete(workItem.id, Failed)
             .map(_ => Done)
       }
