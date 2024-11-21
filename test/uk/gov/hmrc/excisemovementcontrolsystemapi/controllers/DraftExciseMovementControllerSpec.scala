927e1a1645572e0b09598b8c72c59eb6ae20021b
RobertLaverick <10117445+RobertLaverick@users.noreply.github.com>
2024-11-21 18:02:01 +0000

    am I barking up the wrong tree

--- test/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/DraftExciseMovementControllerSpec.scala
+++ test/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/DraftExciseMovementControllerSpec.scala
@@ -193,11 +193,11 @@ class DraftExciseMovementControllerSpec
       verify(auditService).auditMessage(any[IEMessage])(any)
     }
 
-    "Send a MessageSubmitted audit event" in {
-      when(auditService.messageSubmitted(any, ))
-
-      verify(auditService).messageSubmited()
-    }
+//    "Send a MessageSubmitted audit event" in {
+//      when(auditService.messageSubmitted(any, ))
+//
+//      verify(auditService).messageSubmited()
+//    }
 
     "sends a failure audit when a message isn't submitted" in {
       when(submissionMessageService.submit(any, any)(any))
