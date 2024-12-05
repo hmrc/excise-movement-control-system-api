eee1d8993b4f6a889db725e1987e860586a05632
RobertLaverick <10117445+RobertLaverick@users.noreply.github.com>
2024-12-05 15:03:45 +0000

    addressed some todos

--- test/uk/gov/hmrc/excisemovementcontrolsystemapi/services/SubmissionMessageServiceSpec.scala
+++ test/uk/gov/hmrc/excisemovementcontrolsystemapi/services/SubmissionMessageServiceSpec.scala
@@ -133,7 +133,7 @@ class SubmissionMessageServiceSpec extends PlaySpec with ScalaFutures with Eithe
     }
     "submission fails"    should {
       //TODO: EISErrorResponseDetails!!!!!!
-      "return an EISErrorResponsePresentation" in {
+      "return an EISErrorResponseDetails" in {
         when(ernSubmissionRepository.save(any)).thenReturn(Future.successful(Done))
 
         val testError =
