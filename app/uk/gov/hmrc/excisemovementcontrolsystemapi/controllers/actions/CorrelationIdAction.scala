042b5c94f1a7f74e1c2ef4a39e032ecd30acf5e2
RobertLaverick <10117445+RobertLaverick@users.noreply.github.com>
2025-01-08 10:57:41 +0000

    working correlationId action for parsedXmlRequest

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/actions/CorrelationIdAction.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/actions/CorrelationIdAction.scala
@@ -1,14 +1,41 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.excisemovementcontrolsystemapi.controllers.actions
 
 import play.api.mvc.ActionTransformer
 import uk.gov.hmrc.excisemovementcontrolsystemapi.models.auth.ParsedXmlRequest
+import uk.gov.hmrc.excisemovementcontrolsystemapi.services.HttpHeader
 
+import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 
-class CorrelationIdAction()(implicit val executionContext: ExecutionContext) extends ActionTransformer[ParsedXmlRequest, ParsedXmlRequest] {
+class CorrelationIdAction()(implicit val executionContext: ExecutionContext)
+    extends ActionTransformer[ParsedXmlRequest, ParsedXmlRequest] {
 
-  override def transform[A](request: ParsedXmlRequest[A]): Future[ParsedXmlRequest[A]] = {
-    Future.successful(request)
-  }
+  private def generateCorrelationId(): String = UUID.randomUUID().toString
+
+  override def transform[A](request: ParsedXmlRequest[A]): Future[ParsedXmlRequest[A]] =
+    if (!request.headers.hasHeader(HttpHeader.xCorrelationId)) {
+      val amendedHeaders = request.headers.add(HttpHeader.xCorrelationId -> generateCorrelationId())
+      val amendedRequest = request.withHeaders(amendedHeaders)
+      val newRequest     = request.copy(request = request.request.copy(request = amendedRequest))
+      Future.successful(newRequest)
+    } else {
+      Future.successful(request)
+    }
 
 }
--- test/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/actions/CorrelationIdActionSpec.scala
+++ test/uk/gov/hmrc/excisemovementcontrolsystemapi/controllers/actions/CorrelationIdActionSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.excisemovementcontrolsystemapi.controllers.actions
 
 import org.scalatest.{BeforeAndAfterEach, EitherValues}
@@ -12,14 +28,17 @@ import uk.gov.hmrc.excisemovementcontrolsystemapi.services.HttpHeader
 
 import java.util.UUID
 import scala.concurrent.ExecutionContext
+import scala.util.matching.Regex
 
-class CorrelationIdActionSpec  extends PlaySpec
-  with ScalaFutures
-  with EitherValues
-  with BeforeAndAfterEach
-  with TestXml {
+class CorrelationIdActionSpec
+    extends PlaySpec
+    with ScalaFutures
+    with EitherValues
+    with BeforeAndAfterEach
+    with TestXml {
 
   implicit val ec: ExecutionContext = ExecutionContext.Implicits.global
+  val uuidRegex: Regex              = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$".r
 
   "refine" should {
     "return a ParsedXmlRequest with existing correlationId when one is provided" in {
@@ -34,14 +53,17 @@ class CorrelationIdActionSpec  extends PlaySpec
       val enrolmentRequest = EnrolmentRequest(
         FakeRequest()
           .withHeaders(FakeHeaders(headers))
-          .withBody(xml.XML.loadString(xmlStr)), Set("ern"), UserDetails("123", "abc"))
+          .withBody(xml.XML.loadString(xmlStr)),
+        Set("ern"),
+        UserDetails("123", "abc")
+      )
 
       val message = IE815Message.createFromXml(IE815)
 
       val correlationIdAction = new CorrelationIdAction
 
       val inputRequest =
-        ParsedXmlRequest(enrolmentRequest, message,  Set("ern"), UserDetails("123", "abc"))
+        ParsedXmlRequest(enrolmentRequest, message, Set("ern"), UserDetails("123", "abc"))
 
       val result = correlationIdAction.transform(inputRequest).futureValue
 
@@ -53,26 +75,25 @@ class CorrelationIdActionSpec  extends PlaySpec
           | <body></body>
           |</IE815>""".stripMargin
 
-      val testCorrelationId = "testCorrelationId"
-      val headers           = Seq(HttpHeader.xCorrelationId -> testCorrelationId)
-
       val enrolmentRequest = EnrolmentRequest(
         FakeRequest()
-          .withHeaders()
-          .withBody(xml.XML.loadString(xmlStr)), Set("ern"), UserDetails("123", "abc"))
+          .withBody(xml.XML.loadString(xmlStr)),
+        Set("ern"),
+        UserDetails("123", "abc")
+      )
 
       val message = IE815Message.createFromXml(IE815)
 
       val correlationIdAction = new CorrelationIdAction
 
       val inputRequest =
-        ParsedXmlRequest(enrolmentRequest, message,  Set("ern"), UserDetails("123", "abc"))
+        ParsedXmlRequest(enrolmentRequest, message, Set("ern"), UserDetails("123", "abc"))
 
       val result = correlationIdAction.transform(inputRequest).futureValue
 
-      val expectedRequest =
-        ParsedXmlRequest(enrolmentRequest, message,  Set("ern"), UserDetails("123", "abc"))
-      result mustBe ???
+      //Question before merge - do we want to confirm more of the internal details are preserved?
+      result.headers.get(HttpHeader.xCorrelationId).isDefined mustBe true
+      uuidRegex.matches(result.headers.get(HttpHeader.xCorrelationId).get) mustBe true
     }
   }
 }
