c901327b7d6de719cfc662515264d43104cb1a5e
Nemanja Tomanovic <nemanja.tomanovic@gmail.com>
2025-02-03 10:17:21 +0000

    EMCS-749: Added tests for model's writes method. Defined models for auditing.

--- test/uk/gov/hmrc/excisemovementcontrolsystemapi/models/auditing/MovementSavedAuditInfoSpec.scala
+++ test/uk/gov/hmrc/excisemovementcontrolsystemapi/models/auditing/MovementSavedAuditInfoSpec.scala
@@ -1,11 +1,34 @@
 package uk.gov.hmrc.excisemovementcontrolsystemapi.models.auditing
 
 import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.{JsArray, Json}
+
+import java.util.UUID
 
 class MovementSavedAuditInfoSpec extends PlaySpec {
 
   "MovementSavedSuccessAuditInfo.writes" should {
-    "serialise jobId as null when None is provided" in {}
+    "serialise jobId as null when None is provided" in {
+
+      val batchId                           = UUID.randomUUID().toString
+      val messageProcessingSuccessAuditInfo =
+        MovementSavedSuccessAuditInfo()
+
+      val expectedResult = Json.obj(
+        "exciseRegistrationNumber" -> "ern",
+        "messagesAvailable"        -> 10,
+        "messagesInBatch"          -> 10,
+        "messages"                 -> JsArray.empty,
+        "processingStatus"         -> "Success",
+        "batchId"                  -> batchId,
+        "jobId"                    -> JsNull
+      )
+
+      val output = Json.toJson(messageProcessingSuccessAuditInfo)
+
+      output mustBe expectedResult
+      (output \ "jobId").get mustBe JsNull
+    }
   }
 
   "MovementSavedFailureAuditInfo.writes" should {
