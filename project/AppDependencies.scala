0d8e7e4473043f9c54fd601cdea72610ebcc87cb
Richy Jassal <20478717+jassalrichy@users.noreply.github.com>
2025-06-04 11:51:04 +0100

    [CS-6485] - resolve bobby rule violations

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.10.0"
+  private val bootstrapVersion = "9.13.0"
   val dispatchVersion          = "1.2.0"
   lazy val mongoVersion        = "1.7.0"
   val pekkoVersion = "1.0.3"
@@ -13,7 +13,7 @@ object AppDependencies {
     "javax.xml.bind"     % "jaxb-api"                          % "2.3.1",
     "com.beachape"      %% "enumeratum-play-json"              % "1.8.0",
     "org.typelevel"     %% "cats-core"                         % "2.10.0",
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30"      % "3.0.0"
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30"      % "4.0.0"
   )
 
   val test: Seq[ModuleID]      = Seq(
