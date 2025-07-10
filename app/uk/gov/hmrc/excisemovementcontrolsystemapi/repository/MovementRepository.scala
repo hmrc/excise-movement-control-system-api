a4d9ebe1b0d50d6e80945164db701a2d2cef4d2d
nickg <148371638+nickg-hmrc@users.noreply.github.com>
2025-07-10 17:37:44 +0100

    CS-6769: Don't use consignee ID when filtering for draft movement records
    This should make it match the duplication checking logic in Core so we
    don't have the problem where movements are saved successfully by Core
    and then rejected as being duplicates by EMCS API
    
    Also, rename some functions and values to describe them better,
    and some minor refactoring

--- app/uk/gov/hmrc/excisemovementcontrolsystemapi/repository/MovementRepository.scala
+++ app/uk/gov/hmrc/excisemovementcontrolsystemapi/repository/MovementRepository.scala
@@ -72,8 +72,11 @@ class MovementRepository @Inject() (
     val filters = Seq(
       Some(equal("consignorId", movement.consignorId)),
       Some(equal("localReferenceNumber", movement.localReferenceNumber)),
-      Some(not(exists("administrativeReferenceCode"))),
-      movement.consigneeId.map(consignee => equal("consigneeId", consignee))
+      Some(not(exists("administrativeReferenceCode")))
+      // consigneeId is not included here
+      // This should make it match the duplication checking logic in Core so we
+      // don't have the problem where movements are saved successfully by Core
+      // and then rejected as being duplicates by EMCS API - for more info see CS-6769
     )
     collection
       .find(
